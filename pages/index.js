ddb147e5eeb835f0c73c9cc1f133516de7249181
wintermutt <wintermutt@skepticalimagination.com>
2020-11-24 13:42:28 -0500

    Converted component-level css to styled-jsx.

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,4 @@
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
 import { useSelector, useDispatch } from 'react-redux'
 import gameSlice from '../model/slice'
 
@@ -10,28 +9,152 @@ export default function Home() {
   const { increment } = gameSlice.actions
 
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Jones Redux</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    <>
+      <div className="container">
+        <Head>
+          <title>Jones Redux</title>
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Jones Redux
-        </h1>
+        <main>
+          <h1>Jones Redux</h1>
 
-        <p className={styles.description}>
-          Counter: { counter }
-        </p>
+          <p className="description">
+            Counter: { counter }
+          </p>
 
-        <button onClick={() => dispatch(increment())}>
-          Increment
-        </button>
-      </main>
+          <button onClick={() => dispatch(increment())}>
+            Increment
+          </button>
+        </main>
 
-      <footer className={styles.footer}>
-      </footer>
-    </div>
+        <footer></footer>
+      </div>
+
+      <style jsx>{`
+        .container {
+          min-height: 100vh;
+          padding: 0 0.5rem;
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+          align-items: center;
+        }
+
+        main {
+          padding: 5rem 0;
+          flex: 1;
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+          align-items: center;
+        }
+
+        footer {
+          width: 100%;
+          height: 100px;
+          border-top: 1px solid #eaeaea;
+          display: flex;
+          justify-content: center;
+          align-items: center;
+        }
+
+        footer img {
+          margin-left: 0.5rem;
+        }
+
+        footer a {
+          display: flex;
+          justify-content: center;
+          align-items: center;
+        }
+
+        h1 a {
+          color: #0070f3;
+          text-decoration: none;
+        }
+
+        h1 a:hover,
+        h1 a:focus,
+        h1 a:active {
+          text-decoration: underline;
+        }
+
+        h1 {
+          margin: 0;
+          line-height: 1.15;
+          font-size: 4rem;
+        }
+
+        h1,
+        .description {
+          text-align: center;
+        }
+
+        .description {
+          line-height: 1.5;
+          font-size: 1.5rem;
+        }
+
+        .code {
+          background: #fafafa;
+          border-radius: 5px;
+          padding: 0.75rem;
+          font-size: 1.1rem;
+          font-family: Menlo, Monaco, Lucida Console, Liberation Mono, DejaVu Sans Mono,
+            Bitstream Vera Sans Mono, Courier New, monospace;
+        }
+
+        .grid {
+          display: flex;
+          align-items: center;
+          justify-content: center;
+          flex-wrap: wrap;
+          max-width: 800px;
+          margin-top: 3rem;
+        }
+
+        .card {
+          margin: 1rem;
+          flex-basis: 45%;
+          padding: 1.5rem;
+          text-align: left;
+          color: inherit;
+          text-decoration: none;
+          border: 1px solid #eaeaea;
+          border-radius: 10px;
+          transition: color 0.15s ease, border-color 0.15s ease;
+        }
+
+        .card:hover,
+        .card:focus,
+        .card:active {
+          color: #0070f3;
+          border-color: #0070f3;
+        }
+
+        .card h3 {
+          margin: 0 0 1rem 0;
+          font-size: 1.5rem;
+        }
+
+        .card p {
+          margin: 0;
+          font-size: 1.25rem;
+          line-height: 1.5;
+        }
+
+        .logo {
+          height: 1em;
+        }
+
+        @media (max-width: 600px) {
+          .grid {
+            width: 100%;
+            flex-direction: column;
+          }
+        }
+      `}</style>
+    </>
   )
 }
