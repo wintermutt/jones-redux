b87ac1bd5c32bc16ca94a93328269756f58c8bb4
wintermutt <wintermutt@skepticalimagination.com>
2020-11-26 17:22:19 -0500

    Added jobs list to initial state.

--- model/slice.js
+++ model/slice.js
@@ -34,7 +34,64 @@ export default createSlice({
     week: 0,
     timeLeft: 60,
     position: 2,
-    inside: false
+    inside: false,
+    jobs: {
+      'Discount Store': [
+        {name: 'Clerk', wage: 4},
+        {name: 'Assistant Manager', wage: 7},
+        {name: 'Manager', wage: 8}
+      ],
+      'Burger Joint': [
+        {name: 'Cook', wage: 4},
+        {name: 'Clerk', wage: 6},
+        {name: 'Assistant Manager', wage: 7},
+        {name: 'Manager', wage: 8}
+      ],
+      'Clothing Store': [
+        {name: 'Salesperson', wage: 8},
+        {name: 'Assistant Manager', wage: 10},
+        {name: 'Manager', wage: 14}
+      ],
+      'Appliance Store': [
+        {name: 'Salesperson', wage: 6},
+        {name: 'Electronics Repair', wage: 4},
+        {name: 'Manager', wage: 16}
+      ],
+      'University': [
+        {name: 'Janitor', wage: 4},
+        {name: 'Teacher', wage: 13},
+        {name: 'Professor', wage: 24}
+      ],
+      'Factory': [
+        {name: 'Janitor', wage: 8},
+        {name: 'Assembly Worker', wage: 9},
+        {name: 'Secretary', wage: 10},
+        {name: "Machinist's Helper", wage: 12},
+        {name: 'Executive Secretary', wage: 21},
+        {name: 'Machinist', wage: 22},
+        {name: 'Department Manager', wage: 26},
+        {name: 'Engineer', wage: 27},
+        {name: 'General Manager', wage: 30}
+      ],
+      'Bank': [
+        {name: 'Janitor', wage: 7},
+        {name: 'Teller', wage: 12},
+        {name: 'Assistant Manager', wage: 16},
+        {name: 'Manager', wage: 22},
+        {name: 'Investment Broker', wage: 26}
+      ],
+      'Grocery Store': [
+        {name: 'Janitor', wage: 6},
+        {name: 'Checker', wage: 9},
+        {name: 'Butcher', wage: 14},
+        {name: 'Assistant Manager', wage: 18},
+        {name: 'Manager', wage: 21}
+      ],
+      'Rent Office': [
+        {name: 'Groundskeeper', wage: 7},
+        {name: 'Apartment Manager', wage: 9}
+      ]
+    }
   },
   reducers: {
     moveTo(state, action) {
