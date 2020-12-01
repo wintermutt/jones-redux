dfa6ff5388da9ce7eef1b5994dcf63e56c681746
wintermutt <wintermutt@skepticalimagination.com>
2020-12-01 18:43:26 -0500

    Improved button look and feel.

--- components/Dialog.js
+++ components/Dialog.js
@@ -144,15 +144,31 @@ export default function Dialog() {
         }
 
         button {
+          position: relative;
           font-family: 'Press Start 2P';
           font-size: 10px;
           text-transform: uppercase;
           padding: 8px;
-          margin-left: 5px;
+          margin-left: 10px;
           background: green;
-          color: white;
+          color: rgb(204, 224, 204);
           border-width: 3px;
-          border-color: rgb(129, 190, 129) rgb(6, 97, 6) rgb(6, 97, 6) rgb(129, 190, 129);
+          border-color: rgb(129, 190, 129) rgb(59, 168, 59) rgb(59, 168, 59) rgb(129, 190, 129);
+          outline: none;
+        }
+
+        button:before {
+          content: '';
+          position: absolute;
+          top: -5px;
+          left: -5px;
+          bottom: -5px;
+          right: -5px;
+          border: 2px solid black;
+        }
+
+        button:active {
+          filter: invert(100%)
         }
       `}</style>
     </>
