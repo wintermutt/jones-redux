04a8eb4370e9cab45fa399c699ccebeae453eb5b
wintermutt <wintermutt@skepticalimagination.com>
2020-12-07 20:05:39 -0500

    Removed redundant code.

--- components/Portrait.js
+++ components/Portrait.js
@@ -2,7 +2,7 @@ import { useSelector } from 'react-redux'
 import { getCurrentBuilding } from '../state/game'
 
 export default function Portrait() {
-  let {portrait} = useSelector(state => getCurrentBuilding(state))
+  let {portrait} = useSelector(getCurrentBuilding)
   if (!portrait) portrait = 'employment-office.jpg'
 
   return (
