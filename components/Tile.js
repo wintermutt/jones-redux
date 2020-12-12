8c8a52bf2357b550db6a867ca05a356aa8093c46
wintermutt <wintermutt@skepticalimagination.com>
2020-12-11 19:13:17 -0500

    Disabled movement before weekend window dismissal.

--- components/Tile.js
+++ components/Tile.js
@@ -1,4 +1,5 @@
-import { useDispatch } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
+import { getWeekend } from '../state/ui'
 import { moveTo } from '../state/players'
 import { getBuildingAt } from '../state/buildings'
 import getPositionCSS from '../helpers/getPositionCSS'
@@ -7,11 +8,12 @@ export default function Tile({position, width, height}) {
   const dispatch = useDispatch()
 
   const building = getBuildingAt(position)
+  const weekend = useSelector(getWeekend)
   const {top, left, bottom, right} = getPositionCSS(width, height, position)
 
   return (
     <>
-      <div onClick={() => dispatch(moveTo(position))}>
+      <div onClick={() => !weekend && dispatch(moveTo(position))}>
         {building.name}
       </div>
 
