20442222b4785a55d817a9f1aa2d9b1bcebd6c02
wintermutt <wintermutt@skepticalimagination.com>
2020-12-16 17:37:09 -0500

    Added test for buy thunk.

--- state/players.test.js
+++ state/players.test.js
@@ -1,20 +1,31 @@
 import configureStore from 'redux-mock-store'
 import thunk from 'redux-thunk'
 
+import { buildings } from './static.yaml'
 import reduce, { buy, boughtProduct } from './players'
 import { notEnoughCash } from './actions'
 
 const mockStore = configureStore([thunk])
+const mockStoreWithPlayer = (p) => mockStore({
+  economy: {reading: 3},
+  players: {current: 0, all: [{...p, position: 5}]}
+})
 
 describe('players/thunks', () => {
-  test('buy', () => {
-    const {dispatch, getActions} = mockStore({
-      economy: {reading: 3},
-      players: {current: 0, all: [{position: 5, cash: 0}]}
-    })
+  describe('buy', () => {
+    test('boughtProduct', () => {
+      const product = {...buildings[5].products[0], price: 82}
+      const {dispatch, getActions} = mockStoreWithPlayer({cash: product.price})
 
-    dispatch(buy('Cola'))
-    expect(getActions()).toEqual([notEnoughCash()])
+      dispatch(buy(product.name))
+      expect(getActions()).toEqual([boughtProduct(product)])
+    })
+    test('notEnoughCash', () => {
+      const {dispatch, getActions} = mockStoreWithPlayer({cash: 0})
+
+      dispatch(buy('Cola'))
+      expect(getActions()).toEqual([notEnoughCash()])
+    })
   })
 })
 
