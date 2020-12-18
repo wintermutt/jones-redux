08a5e32809bd9ce77975d161b72707a2a93eb2e6
wintermutt <wintermutt@skepticalimagination.com>
2020-12-18 16:55:38 -0500

    Added tests for leaveBuilding, streamlined mocks for others.

--- state/players.test.js
+++ state/players.test.js
@@ -2,19 +2,26 @@ import configureStore from 'redux-mock-store'
 import thunk from 'redux-thunk'
 
 import { buildings } from './static.yaml'
+
 import reduce, {
   buy, boughtProduct,
   enroll, enrolled,
   relax, relaxed,
-  work, worked
+  work, worked,
+  leaveBuilding
 } from './players'
-import { notEnoughTime, notEnoughCash } from './actions'
 
+import {
+  notEnoughTime,
+  notEnoughCash,
+  leftBuilding,
+  turnEnded
+} from './actions'
+
+const {arrayContaining} = expect
 const mockStore = configureStore([thunk])
-const testStore = (player) => mockStore({
-  economy: {reading: 3},
-  players: {current: 0, all: [player]}
-})
+const economy = {reading: 3}
+const lonePlayer = player => ({current: 0, all: [player]})
 
 describe('players/thunks', () => {
   describe('buy', () => {
@@ -22,14 +29,20 @@ describe('players/thunks', () => {
 
     test('boughtProduct', () => {
       const product = {...buildings[5].products[0], price: 82}
-      const {dispatch, getActions} = testStore({position, cash: product.price})
+      const {dispatch, getActions} = mockStore({
+        economy,
+        players: lonePlayer({position, cash: product.price})
+      })
 
       dispatch(buy(product.name))
       expect(getActions()).toEqual([boughtProduct(product)])
     })
 
     test('notEnoughCash', () => {
-      const {dispatch, getActions} = testStore({position, cash: 0})
+      const {dispatch, getActions} = mockStore({
+        economy,
+        players: lonePlayer({position, cash: 0})
+      })
 
       dispatch(buy('Cola'))
       expect(getActions()).toEqual([notEnoughCash()])
@@ -40,14 +53,20 @@ describe('players/thunks', () => {
     const position = 9
 
     test('enrolled', () => {
-      const {dispatch, getActions} = testStore({position, cash: 52})
+      const {dispatch, getActions} = mockStore({
+        economy,
+        players: lonePlayer({position, cash: 52})
+      })
 
       dispatch(enroll())
       expect(getActions()).toEqual([enrolled(52)])
     })
 
     test('notEnoughCash', () => {
-      const {dispatch, getActions} = testStore({position, cash: 0})
+      const {dispatch, getActions} = mockStore({
+        economy,
+        players: lonePlayer({position, cash: 0})
+      })
 
       dispatch(enroll())
       expect(getActions()).toEqual([notEnoughCash()])
@@ -56,14 +75,18 @@ describe('players/thunks', () => {
 
   describe('relax', () => {
     test('relaxed', () => {
-      const {dispatch, getActions} = testStore({timeLeft: 60})
+      const {dispatch, getActions} = mockStore({
+        players: lonePlayer({timeLeft: 60})
+      })
 
       dispatch(relax())
       expect(getActions()).toEqual([relaxed()])
     })
 
     test('notEnoughTime', () => {
-      const {dispatch, getActions} = testStore({timeLeft: 0})
+      const {dispatch, getActions} = mockStore({
+        players: lonePlayer({timeLeft: 0})
+      })
 
       dispatch(relax())
       expect(getActions()).toEqual([notEnoughTime()])
@@ -74,17 +97,41 @@ describe('players/thunks', () => {
     const job = {employer: "Mr. Ronald's", name: 'Cook', wage: 5}
 
     test('worked', () => {
-      const {dispatch, getActions} = testStore({job, timeLeft: 60})
+      const {dispatch, getActions} = mockStore({
+        players: lonePlayer({job, timeLeft: 60})
+      })
 
       dispatch(work())
       expect(getActions()).toEqual([worked({earnings: 40, timeSpent: 6})])
     })
 
     test('notEnoughTime', () => {
-      const {dispatch, getActions} = testStore({job, timeLeft: 0})
+      const {dispatch, getActions} = mockStore({
+        players: lonePlayer({job, timeLeft: 0})
+      })
 
       dispatch(work())
       expect(getActions()).toEqual([notEnoughTime()])
+    })
+  })
+
+  describe('leaveBuilding', () => {
+    test('leftBuilding', () => {
+      const {dispatch, getActions} = mockStore({
+        players: lonePlayer({timeLeft: 60})
+      })
+
+      dispatch(leaveBuilding())
+      expect(getActions()).toEqual([leftBuilding()])
+    })
+
+    test('leftBuilding, turnEnded', () => {
+      const {dispatch, getActions} = mockStore({
+        players: lonePlayer({timeLeft: 0})
+      })
+
+      dispatch(leaveBuilding())
+      expect(getActions()).toEqual(arrayContaining([leftBuilding(), turnEnded()]))
     })
   })
 })
@@ -92,7 +139,7 @@ describe('players/thunks', () => {
 describe('players/reducers', () => {
   test('boughtProduct', () => {
     const {all: [player]} = reduce(
-      {current: 0, all: [{cash: 100, hungry: true}]},
+      lonePlayer({cash: 100, hungry: true}),
       boughtProduct({price: 10, feeds: true})
     )
 
@@ -102,7 +149,7 @@ describe('players/reducers', () => {
 
   test('enrolled', () => {
     const {all: [player]} = reduce(
-      {current: 0, all: [{cash: 52, enrollments: 0}]},
+      lonePlayer({cash: 52, enrollments: 0}),
       enrolled(52)
     )
 
@@ -112,7 +159,7 @@ describe('players/reducers', () => {
 
   test('relaxed', () => {
     const {all: [player]} = reduce(
-      {current: 0, all: [{timeLeft: 60}]},
+      lonePlayer({timeLeft: 60}),
       relaxed()
     )
 
@@ -121,11 +168,20 @@ describe('players/reducers', () => {
 
   test('worked', () => {
     const {all: [player]} = reduce(
-      {current: 0, all: [{cash: 0, timeLeft: 60}]},
+      lonePlayer({cash: 0, timeLeft: 60}),
       worked({earnings: 40, timeSpent: 6})
     )
 
     expect(player.cash).toEqual(40)
     expect(player.timeLeft).toEqual(54)
+  })
+
+  test('leftBuilding', () => {
+    const {all: [player]} = reduce(
+      lonePlayer({inside: true}),
+      leftBuilding()
+    )
+
+    expect(player.inside).toEqual(false)
   })
 })
